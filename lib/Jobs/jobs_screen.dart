a5734901955a651ec7de24012b8a88a0b7120512
TommyBom <fitlaunch@comcast.net>
2022-10-19 20:19:26 -0700

    bottom nav bar widget added

--- lib/Jobs/jobs_screen.dart
+++ lib/Jobs/jobs_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../Widgets/bottom_nav_bar.dart';
 
 class JobScreen extends StatefulWidget {
   const JobScreen({Key? key}) : super(key: key);
@@ -8,14 +11,46 @@ class JobScreen extends StatefulWidget {
 }
 
 class _JobScreenState extends State<JobScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Jobs Screen'),
-      ),
-      body: const Center(
-        child: Text('What Up Dog'),
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [Colors.deepOrange.shade300, Colors.blueAccent],
+        begin: Alignment.centerLeft,
+        end: Alignment.centerRight,
+        stops: const [0.2, 0.9],
+      )),
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBarForApp(indexNum: 0),
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text('Job Screen'),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.deepOrange.shade300, Colors.blueAccent],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                stops: const [0.2, 0.9],
+              ),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'What Up Dog?',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
